560c7b455459f610edeacb152650df2ea45e41c5
Alexei Bobrik <fingerloom2004@gmail.com>
2025-03-30 23:19:59 +0300

    check request availability

--- force-app/main/default/lwc/portalSubmitAccomodationRequestBtn/portalSubmitAccomodationRequestBtn.js
+++ force-app/main/default/lwc/portalSubmitAccomodationRequestBtn/portalSubmitAccomodationRequestBtn.js
@@ -1,16 +1,27 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
+import isRequestSubmissionAvailable from '@salesforce/apex/AccomodationRequestController.isRequestSubmissionAvailable';
+import isRegistrationAvailable from '@salesforce/apex/AccomodationRequestController.isRegistrationAvailable';
 import createAccommodationRequest from '@salesforce/apex/AccomodationRequestController.createAccommodationRequest';
 import Toast from 'lightning/toast';
 
 export default class PortalSubmitAccomodationRequestBtn extends LightningElement {
     error = undefined;
 
+    isRegistrationAvailable = false;
+    isRequestSubmissionAvailable = false;
+
+    connectedCallback() {
+        this.checkButtonAvailability();
+    }
+
     async handleRequest() {
         this.dispatchEvent(new CustomEvent('loading', { detail: { isLoading: true } }));
         try {
             await createAccommodationRequest();
             this.showToast('Успешно', 'Заявка успешно подана', 'success');
+            this.checkButtonAvailability();
             this.dispatchEvent(new CustomEvent('requestcreated'));
+
             this.error = undefined;
         } catch (error) {
             this.error = error.body.message;
@@ -21,11 +32,16 @@ export default class PortalSubmitAccomodationRequestBtn extends LightningElement
     }
 
     showToast(label, message, variant) {
-        Toast.show({
-            label, 
-            message, 
-            variant,
-            mode: 'dismissible'
-        });
+        Toast.show({ label, message, variant, mode: 'dismissible' });
+    }
+
+    async checkButtonAvailability() {
+        this.isRegistrationAvailable = await isRegistrationAvailable();
+        this.isRequestSubmissionAvailable = await isRequestSubmissionAvailable();
+    }
+
+    @api
+    get isButtonDisabled() {
+        return !this.isRegistrationAvailable || !this.isRequestSubmissionAvailable;
     }
 }
