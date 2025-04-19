acca7b16cc7a05b5e2378dce6edac7c6c85c0f89
Alexei Bobrik <fingerloom2004@gmail.com>
2025-04-19 17:44:59 +0300

    basic social useful work management

---
+++ force-app/main/default/lwc/recalculateStudyHoursAction/recalculateStudyHoursAction.js
@@ -0,0 +1,35 @@
+import { LightningElement, api } from 'lwc';
+import recalculateSingleStudent from '@salesforce/apex/RecalculateStudyHoursActionController.recalculateSingleStudent';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class RecalculateStudyHoursAction extends LightningElement {
+    @api recordId;
+    isExecuting = false;
+
+    @api async invoke() {
+        if (this.isExecuting) return;
+        this.isExecuting = true;
+
+        try {
+            await recalculateSingleStudent({ accountId: this.recordId });
+
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Успех',
+                    message: 'Часы успешно пересчитаны.',
+                    variant: 'success'
+                })
+            );
+        } catch (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Ошибка',
+                    message: error?.body?.message || 'Ошибка при пересчете часов.',
+                    variant: 'error'
+                })
+            );
+        } finally {
+            this.isExecuting = false;
+        }
+    }
+}
